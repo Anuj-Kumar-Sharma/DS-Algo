05355741cc635f19d9381bbfbebe4d776eb00347
Anuj Kumar Sharma <anujkumarsharma@urbanclap.com>
2020-03-14 19:59:52 +0530

    sets added

---
+++ sets/MainClass.java
@@ -0,0 +1,21 @@
+package sets;
+
+import java.util.*;
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		Set<Integer> x = new HashSet<>();
+		x.add(23);
+		x.add(1);
+		x.add(6);
+		
+		Set<Integer> y = new HashSet<>();
+		y.add(1);
+		y.add(12);
+		
+		System.out.println(x.containsAll(y));
+		
+		System.out.println(x);
+	}
+}
