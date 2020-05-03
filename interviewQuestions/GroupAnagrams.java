e4692fffd9be87ee6f66ecf60d6bd0535dbfd5a0
Anuj Kumar Sharma <anujkumarsharma@urbanclap.com>
2020-05-03 23:31:34 +0530

    LeetCode Question GroupAnagrams added

---
+++ interviewQuestions/GroupAnagrams.java
@@ -0,0 +1,31 @@
+package interviewQuestions;
+
+import java.util.*;
+public class GroupAnagrams {
+	
+	public static void main(String[] args) {
+		String strs[] = {"eat", "tea", "tan", "ate", "nat", "bat"};
+		List<List<String>> ans = groupAnagrams(strs);
+		
+		for(List<String> list: ans) {
+			System.out.println(list);
+		}
+	}
+	
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s: strs) {
+            int count[] = new int[26];
+            for(int i = 0; i<s.length(); i++) {
+                count[s.charAt(i)-'a']++;
+            }
+            String key = Arrays.toString(count);
+            List<String> list = map.getOrDefault(key, new LinkedList<String>());
+            
+            list.add(s);
+            map.put(key, list);
+        }
+        
+        return new LinkedList<>(map.values());
+    }
+}
