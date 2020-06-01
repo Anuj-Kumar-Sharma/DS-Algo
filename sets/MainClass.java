ffe9033be339fea4c1b3171c452e966ba1c1001d
Rajiv Ranjan Singh <rajivperfect007@gmail.com>
2020-06-01 05:47:12 +0530

    refactored code in sets and vectorAndStacks file

--- sets/MainClass.java
+++ sets/MainClass.java
@@ -1,6 +1,7 @@
 package sets;
 
 import java.util.*;
+
 public class MainClass {
 
 	public static void main(String[] args) {
@@ -9,13 +10,13 @@ public class MainClass {
 		x.add(23);
 		x.add(1);
 		x.add(6);
-		
+
 		Set<Integer> y = new HashSet<>();
 		y.add(1);
 		y.add(12);
-		
+
 		System.out.println(x.containsAll(y));
-		
+
 		System.out.println(x);
 	}
 }
