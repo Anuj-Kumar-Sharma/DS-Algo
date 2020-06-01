34200735c576dd2c5e7268610fb6bbee4029a5fe
Rajiv Ranjan Singh <rajivperfect007@gmail.com>
2020-06-01 05:44:11 +0530

    refactored code using prettier

--- lists/Pair.java
+++ lists/Pair.java
@@ -4,14 +4,14 @@ public class Pair<X, Y> {
 
 	X x;
 	Y y;
-	
+
 	public Pair(X x, Y y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public void getDescription() {
-		System.out.println(x +" and " + y);
+		System.out.println(x + " and " + y);
 	}
-	
+
 }
