6e9c44d0554bdc652629dc8d58f208b6e58e1b0d
Muddhit Baid <61585314+Mukulbaid63@users.noreply.github.com>
2020-06-01 16:20:50 +0530

    Update MyStack.java

--- vectorAndStacks/MyStack.java
+++ vectorAndStacks/MyStack.java
@@ -7,19 +7,19 @@ public class MyStack<E> {
 	private MyLinkedList<E> ll = new MyLinkedList<>();
 	
 	void push(E e) {
-		ll.add(e);
+		ll.add(e);//add the element in the linked list
 	}
 	
 	E pop() throws Exception {
 		if(ll.isEmpty()) {
-			throw new Exception("Popping from empty stack is not allowed");
+			throw new Exception("Popping from empty stack is not allowed");//handling the empty linked list while trying to pop
 		}
 		return ll.removeLast();
 	}
 	
 	E peek() throws Exception {
 		if(ll.isEmpty()) {
-			throw new Exception("Peeking from empty stack is not allowed");
+			throw new Exception("Peeking from empty stack is not allowed");//handling the empty linked list while trying to peek
 		}
 		return ll.getLast();
 	}
