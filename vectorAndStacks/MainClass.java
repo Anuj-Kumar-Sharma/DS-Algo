b7c435f08386bd007a38e9689421fc8c87c80d99
Muddhit Baid <61585314+Mukulbaid63@users.noreply.github.com>
2020-06-01 18:03:02 +0530

    Update MainClass.java

--- vectorAndStacks/MainClass.java
+++ vectorAndStacks/MainClass.java
@@ -6,11 +6,11 @@ public class MainClass {
 		
 		MyStack<Integer> stack = new MyStack<>();
 		
-		int popped = stack.pop();// delete the top element of the stack
+		int popped = stack.pop();// delete the top element of the stack.
 		
-		System.out.println(popped);//print the last popped element
+		System.out.println(popped);//print the last popped element of the stack.
 		
-		System.out.println(stack.peek());//print the top element of the stack
+		System.out.println(stack.peek());//print the top element of the stack.
 	}
 
 }
