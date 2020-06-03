0e658e8e3d2155a1b87df9486d6b29a309a283dd
Dev Kumar <64922424+techdevg@users.noreply.github.com>
2020-06-03 21:27:14 +0530

    Update MyArrayList.java

--- lists/MyArrayList.java
+++ lists/MyArrayList.java
@@ -8,18 +8,18 @@ public class MyArrayList {
 
 	public static void main(String[] args) {
 
-		List<String> fruits = new LinkedList();
+		List<String> fruits = new LinkedList();	//We declared a linked list with object fruits of type string
 		
-		fruits.add("Apple");
-		fruits.add("Orange");
-		fruits.add("Hi");
+		fruits.add("Apple");		// String Apple added to linkedlist fruits
+		fruits.add("Orange");		// String Orange add to linkedlist fruits
+		fruits.add("Hi");		// String Hi added to linkedlist fruits 
 		
-		String temp[] = new String[fruits.size()];
+		String temp[] = new String[fruits.size()];	// declared a string array with the same size of fruits list
 		
-		fruits.toArray(temp);
+		fruits.toArray(temp);		// here linkedlist of type string is converted to array inside temp array
 		
-		for(String e: temp) {
-			System.out.println(e);
+		for(String e: temp) {	 // putting string of every position in temp array to String e using for each loop
+			System.out.println(e);	// printing of arrays of string of array temp through use of string e
 		}
 	}
 
