0cf79b82c4895f25799177ae336dbf4f51b55d55
Rajiv Singh <rajivperfect007@gmail.com>
2020-06-05 12:57:48 +0530

    refactored code using prettier (#13)
    * refactored code using prettier
    
    * refactored code in sets and vectorAndStacks file

--- interviewQuestions/SubarrayWithZeroSum.java
+++ interviewQuestions/SubarrayWithZeroSum.java
@@ -6,39 +6,40 @@ package interviewQuestions;
  */
 
 import java.util.*;
+
 public class SubarrayWithZeroSum {
 
 	public static void main(String[] args) {
-		
-		int[] a = {2, 1, 3, -4, -2};
+
+		int[] a = { 2, 1, 3, -4, -2 };
 		int k = -3;
 		boolean found = false;
-		
-//		for(int i = 0; i<a.length; i++) {
-//			int sum = 0;
-//			for(int j = i; j<a.length; j++) {
-//				sum += a[j];
-//				if(sum == 0) {
-//					found = true;
-//					break;
-//				}
-//			}
-//			if(found) break;
-//		}
-		
+
+		// for(int i = 0; i<a.length; i++) {
+		// int sum = 0;
+		// for(int j = i; j<a.length; j++) {
+		// sum += a[j];
+		// if(sum == 0) {
+		// found = true;
+		// break;
+		// }
+		// }
+		// if(found) break;
+		// }
+
 		Set<Integer> set = new HashSet<>();
 		int sum = 0;
-		for(int element : a) {
+		for (int element : a) {
 			set.add(sum);
 			sum += element;
-			if(set.contains(sum-k)) {
+			if (set.contains(sum - k)) {
 				found = true;
 				break;
 			}
 		}
-		
-		System.out.println("found "+ found);
-		
+
+		System.out.println("found " + found);
+
 	}
 
 }
--- priorityQueues/MainClass.java
+++ priorityQueues/MainClass.java
@@ -5,21 +5,21 @@ import java.util.PriorityQueue;
 public class MainClass {
 
 	public static void main(String[] args) {
-		
+
 		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		
+
 		pq.add(45);
 		pq.add(12);
 		pq.add(1);
 		pq.add(100);
-		
+
 		System.out.println(pq);
-		
+
 		System.out.println(pq.remove());
 		System.out.println(pq.remove());
 		System.out.println(pq.remove());
 		System.out.println(pq.remove());
-		
+
 	}
 
 }
