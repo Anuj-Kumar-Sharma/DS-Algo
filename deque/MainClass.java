b6a6c52135bbf88682f9a3f1ca3c16d44247e9cf
Muddhit Baid <61585314+Mukulbaid63@users.noreply.github.com>
2020-06-06 16:36:10 +0530

    Update MainClass.java

--- deque/MainClass.java
+++ deque/MainClass.java
@@ -8,10 +8,12 @@ public class MainClass {
 	
 		ArrayDeque<Integer> ad = new ArrayDeque<>();
 		
+		//method to push the element in the doubly ended queue
 		ad.push(12);
 		ad.push(23);
 		ad.push(34);
 		
+		//method to print the popped element at the beginning of a doubly ended queue
 		System.out.println(ad.pop());
 		System.out.println(ad.pop());
 		
