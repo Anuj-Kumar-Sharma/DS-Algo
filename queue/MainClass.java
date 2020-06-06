59d07c2946518978b62295467b87126f86ce67b3
Muddhit Baid <61585314+Mukulbaid63@users.noreply.github.com>
2020-06-06 16:26:10 +0530

    Update MainClass.java

--- queue/MainClass.java
+++ queue/MainClass.java
@@ -5,12 +5,14 @@ public class MainClass {
 	public static void main(String[] args) {
 
 		MyQueue<Integer> mq = new MyQueue<>();
-
+		
+		// method to add element in queue	
 		mq.enqueue(12);
 		mq.enqueue(2);
 		mq.enqueue(123);
 		mq.enqueue(45);
-
+		
+		//printing the element removed from the beginning of queue 
 		System.out.println(mq.dequeue());
 		System.out.println(mq.dequeue());
 		System.out.println(mq.dequeue());
