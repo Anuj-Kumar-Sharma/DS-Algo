da871e7124f1b827450b0e96657470acd64ae7fd
Muddhit Baid <61585314+Mukulbaid63@users.noreply.github.com>
2020-06-07 14:01:58 +0530

    Update MyArrayList.java

--- lists/MyArrayList.java
+++ lists/MyArrayList.java
@@ -9,7 +9,8 @@ public class MyArrayList {
 	public static void main(String[] args) {
 
 		List<String> fruits = new LinkedList();
-
+		
+		//method to add the element in the list
 		fruits.add("Apple");
 		fruits.add("Orange");
 		fruits.add("Hi");
