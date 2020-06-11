218e4ad013f9a040b7954327356dfc215ba34b0f
Dev Kumar <64922424+techdevg@users.noreply.github.com>
2020-06-11 12:28:35 +0530

    Update Pair.java

--- lists/Pair.java
+++ lists/Pair.java
@@ -1,27 +1,27 @@
 package lists;
 
-// we have used generics of type X and Y
+//we have used generics of type X and Y
 public class Pair<X, Y> {		
 
-	// declared varible x of type X and y of type Y
+	//declared varible x of type X and y of type Y
 	X x;	
 	Y y;		
 	
-	// created a constructor Pair with two parameteric variable x and y of type X and Y respectively
+	//created a constructor Pair with two parameteric variable x and y of type X and Y respectively
 	
 	public Pair(X x, Y y) {	
 		
-		// assigning the value global variable x and y with 'this' keyword
+		//assigning the value global variable x and y with 'this' keyword
 		
 		this.x = x;		
 		this.y = y;		
 	}
 	
-	// created method getDescription with public access modifier and void return type void
+	//created method getDescription with public access modifier and void return type void
 	
 	public void getDescription() {		
 		
-		// printing value of x and y of global variable
+		//printing value of x and y of global variable
 		
 		System.out.println(x +" and " + y);	
 
