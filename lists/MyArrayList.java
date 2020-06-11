0b574fccbd1db07e7d348acd7c6bb341134e7b86
Dev Kumar <64922424+techdevg@users.noreply.github.com>
2020-06-11 12:27:30 +0530

    Update MyArrayList.java

--- lists/MyArrayList.java
+++ lists/MyArrayList.java
@@ -7,33 +7,34 @@ import java.util.List;
 public class MyArrayList {
 
 	public static void main(String[] args) {
-
-		List<String> fruits = new LinkedList();	//We declared a linked list with object fruits of type string
 		
-		fruits.add("Apple");		// String Apple added to linkedlist fruits
-		fruits.add("Orange");		// String Orange add to linkedlist fruits
-		fruits.add("Hi");		// String Hi added to linkedlist fruits 
+		//We declared a linked list with object fruits of type string
+
+		List<String> fruits = new LinkedList();	
 		
-		String temp[] = new String[fruits.size()];	// declared a string array with the same size of fruits list
+		//String "Apple", "Orange" and "Hi" added to linkedlist fruits
 		
-		fruits.toArray(temp);		// here linkedlist of type string is converted to array inside temp array
+		fruits.add("Apple");		
+		fruits.add("Orange");		
+		fruits.add("Hi");		
 		
-		for(String e: temp) {	 // putting string of every position in temp array to String e using for each loop
-			System.out.println(e);	// printing of arrays of string of array temp through use of string e
+		//declared a string array with the same size of fruits list
+		
+		String temp[] = new String[fruits.size()];	
+		
+		//here linkedlist of type string is converted to array inside temp array
+		
+		fruits.toArray(temp);		
+		
+		// putting string of every position in temp array to String e using for each loop
+		
+		for(String e: temp) {	 
+			
+			// printing of arrays of string of array temp through use of string e
+			
+			System.out.println(e);	
 
-		List<String> fruits = new LinkedList();
-
-		fruits.add("Apple");
-		fruits.add("Orange");
-		fruits.add("Hi");
-
-		String temp[] = new String[fruits.size()];
-
-		fruits.toArray(temp);
-
-		for (String e : temp) {
-			System.out.println(e);
-		}
+		
 	}
 
 }
