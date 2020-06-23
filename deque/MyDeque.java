41d67b95aae8157b05fa63f4d10543658f3fc956
Dev Kumar <64922424+techdevg@users.noreply.github.com>
2020-06-24 00:11:38 +0530

    Update MyDeque.java (#26)
    Added more functions/methods using linked list to MyDeque

--- deque/MyDeque.java
+++ deque/MyDeque.java
@@ -1,45 +1,86 @@
 package deque;
 
 public class MyDeque<E> {
-
+	
 	Node<E> head, tail;
-
-	public void addToHead(E data) {
-		Node<E> toAdd = new Node<>(data);
-		if (head == null) {
-			head = tail = toAdd;
-		} else {
-			head.next = toAdd;
-			toAdd.prev = head;
-			head = head.next;
-		}
-	}
-
-	public E removeLast() {
-		if (head == null) {
-			return null;
-		}
-		Node<E> toRemove = tail;
-		if (tail.next == null) {
-			tail = head = null;
-			return toRemove.data;
-		}
-		tail = tail.next;
-		tail.prev = null;
-		if (tail == null) {
-			head = null;
-		}
-		return toRemove.data;
-	}
-
-	public static class Node<E> {
-		E data;
+	
+	public static class Node<E>{
 		Node<E> next, prev;
-
+		E data;
 		public Node(E data) {
 			this.data = data;
 			this.next = this.prev = null;
 		}
 	}
-
+	
+	public boolean isEmpty() {
+		return head == null;
+	}
+	
+	
+	public void addFirst(E data) {
+		Node<E> toAdd = new Node<>(data);
+ 		if(head == null) {
+			head = tail = toAdd;
+		}
+ 		head.next = toAdd;
+ 		toAdd.prev = head;
+ 		head = toAdd;
+	}
+	
+	public void addLast(E data) {
+		Node<E> toAdd = new Node<>(data);
+		if(tail == null) {
+			tail = head = toAdd;
+			return;
+		}
+		tail.prev = toAdd;
+		toAdd.next = tail;
+		tail = toAdd;
+	}
+	
+	public E removeLast() {
+		if(head == null) {
+			return null;
+		}
+		if(tail.next == null) {
+			Node<E> toRemove = tail;
+			tail = head = null;
+			return toRemove.data;
+		}
+		Node<E> toRemove = tail;
+		tail = tail.next;
+		tail.prev = null;
+		return toRemove.data;
+	}
+	public E removeFirst() {
+		if(head == null) {
+			return null;
+		}
+		if(head.prev == null) {
+			Node<E> toRemove = head;
+			head = tail = null;
+			return toRemove.data;
+		}
+		Node<E> toRemove = head;
+		head = head.prev;
+		head.next = null;
+		return toRemove.data;
+	}
+	
+	void push(E data) {
+		addFirst(data);
+	}
+	
+	E pop() {
+		return removeFirst();
+	}
+	
+	public E peekLast() {
+		return tail.data;
+	}
+	
+	public E peekFirst() {
+		return head.data;
+	}
 }
