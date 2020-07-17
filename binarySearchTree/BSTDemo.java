fa77e34f25226c77eae0b6cd35fc71dc0af5b8e3
abhikarkamkar <abhishek.karkamkar@gmail.com>
2020-07-17 14:04:16 +0530

    bst

---
+++ binarySearchTree/BSTDemo.java
@@ -0,0 +1,23 @@
+package com.std;
+
+public class BSTDemo {
+
+    public static void main(String []args){
+
+        BinarySearchTree BSTree = new BinarySearchTree();
+        BSTree.add(5);
+        BSTree.add(4);
+        BSTree.add(8);
+        BSTree.add(3);
+        BSTree.add(6);
+        BSTree.add(7);
+
+        BSTree.preOrder();
+        System.out.println();
+        BSTree.postOrder();
+        System.out.println();
+        BSTree.inOrder();
+
+    }
+
+}
---
+++ binarySearchTree/BinarySearchTree.java
@@ -0,0 +1,77 @@
+package com.std;
+
+public class BinarySearchTree {
+
+    class Node{
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data){
+            this.data = data;
+        }
+    }
+
+    Node root;
+
+    public void add(int data){
+        Node node = new Node(data);
+        root = insert(root, node);
+    }
+
+    private Node insert(Node root,Node node){
+
+        if (root == null){
+            root = node;
+        }
+        else {
+
+            if (node.data < root.data){
+                root.left = insert(root.left,node);
+            }
+            else {
+                root.right = insert(root.right, node);
+            }
+        }
+        return root;
+    }
+
+    public void preOrder(){
+        printPreOrder(root);
+    }
+    public void postOrder(){
+        printPostOrder(root);
+    }
+    public void inOrder(){
+        printInOrder(root);
+    }
+    
+    private void printPreOrder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        printPreOrder(root.left);
+        printPreOrder(root.right);
+    }
+
+    private void printInOrder(Node root){
+        if(root == null){
+            return;
+        }
+
+        printInOrder(root.left);
+        System.out.print(root.data + " ");
+        printInOrder(root.right);
+    }
+
+    private void printPostOrder(Node root){
+        if(root == null){
+            return;
+        }
+        printPostOrder(root.left);
+        printPostOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+}
