a5a9192008106fb2d477fe313b39d88d7216f579
Aman Kumar <amansingh8066@gmail.com>
2020-10-02 11:21:24 +0530

    Added few comments

--- maps/MainClass.java
+++ maps/MainClass.java
@@ -7,20 +7,20 @@ public class MainClass {
 	
 	public static void main(String[] args) {
 	
-//		Map<String, Integer> numbers = new HashMap<>();
+//		Map<String, Integer> numbers = new HashMap<>(); //This is how you create an object of HashMap
 //	
-//		numbers.put("one", 1);
+//		numbers.put("one", 1); // put function helps let you add <E1,E2> pairs in the HashMap
 //		numbers.put("two", 2);
 //		numbers.put("five", 5);
 //		
-//		numbers.remove("two", 4);
+//		numbers.remove("two", 4); // This is how you remove pair from HashMap
 //		
-//		System.out.println(numbers);
+//		System.out.println(numbers);		//Printing contents on HashMap
 //		System.out.println(numbers.keySet());
 //		System.out.println(numbers.values());
 //		System.out.println(numbers.entrySet());
 //		
-//		Set<Entry<String, Integer>> entries = numbers.entrySet();
+//		Set<Entry<String, Integer>> entries = numbers.entrySet(); 
 //		
 //		for(Entry<String, Integer> entry: entries) {
 //			entry.setValue(entry.getValue() * 100);
